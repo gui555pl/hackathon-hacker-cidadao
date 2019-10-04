42f35271ea5b60eb396d967a1221d903812c63c4
Vinícius <vinicius@ensinartecnologia.com.br>
2019-10-04 14:27:25 -0300

    try to center buttons

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Ciodes from './views/Ciodes.vue'
 import Login from './views/Login.vue'
 
--- src/views/Ciodes.vue
+++ src/views/Ciodes.vue
@@ -1,16 +1,19 @@
 <template>
-    <v-container>
-        <v-layout>
-            <v-flex>
-                <v-btn>
-                    Nova ocorrência
-                </v-btn>
-            </v-flex>
-            <v-flex>
-                <v-btn>
-                    Acompanhar ocorrências
-                </v-btn>
-            </v-flex>
+    <v-container  ma-0 pa-0 style="width: 100%; height: 100%;">
+        <v-layout style="border: solid 1px green; height: 100%; width: 100%" row fluid >
+            <v-layout style="border: solid 1px red;" column align-center>
+                <v-flex>
+                    <v-btn>
+                        Nova ocorrência
+                    </v-btn>
+                </v-flex>
+                <v-flex>
+                    <v-btn>
+                        Acompanhar ocorrências
+                    </v-btn>
+                </v-flex>
+            </v-layout>
+                
         </v-layout>
     </v-container>
 </template>

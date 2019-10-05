1c7ec29de4bbd296002719fbb25e27e20cdb80dd
7bec <pedrorochacmr2013@hotmail.com>
2019-10-04 22:49:30 -0300

    Ocorrencia e components

--- src/components/Samu.vue
+++ src/components/Samu.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="width: 100%; height: 100%; background-color: #a1a1a1;">
-        <v-container fill-height>
+        <v-container  style="width: 100%; height:100%;">
             <v-layout column>
                 <h3 class="text-xs-center"> Qtd. de vítimas </h3>
                         <v-flex>
@@ -21,16 +21,16 @@
                             >
                             </v-select>
                         </v-flex>
-                        <h3 class="text-xs-center">Onde foi a lesão</h3>
-                           <v-flex>
+                           <v-flex v-if="temLesao== false">
+                                <h3 class="text-xs-center">Local da lesão</h3>
                             <v-select
-                                v-model="locaLesao"
+                                v-model="localLesao"
                                 :items="items3"
                                 label="Selecione o local da lesão" 
                             >
                             </v-select>
                         </v-flex>
-                       
+                       <v-btn @click="salvar()">SALVAR</v-btn>
 
             </v-layout>
         </v-container>
@@ -48,9 +48,22 @@ export default {
             items3:['Cranio','Face','Pescoço','Dorso','Torax','Abdômem']
         }
     },
+    created(){
+    },
     computed:{
         temLesao(){
-            return this.lesao=='Não houve lesão'
+            return this.lesao=='Não houve lesão' || this.lesao == ''
+
+        }
+    },
+    methods:{
+        salvar(){
+            this.vitimas='',
+            this.items1=['1','2','3','4','5 ou mais'],
+            this.lesao= '',
+            this.items2=['Não houve lesão','Escoriação','Ferimentro','Fratura aberta','Fratura Fechada','Perfuração arma branca','Perfuração arma de fogo'],
+            this.localLesao='',
+            this.items3=['Cranio','Face','Pescoço','Dorso','Torax','Abdômem']
         }
     }
      
--- src/views/Ocorrencia.vue
+++ src/views/Ocorrencia.vue
@@ -1,7 +1,75 @@
 <template>
-    <div>
-        <samu v-if="tipo == 'samu' "> </samu>
-        <CTTU v-if="tipo == 'CTTU' "> </CTTU>
+    <div style="width:100%; height:100%; ">
+        <v-card-text style="background-color: #a1a1a1;">
+            <v-container >
+                <v-layout mt-4 column>
+                    <v-flex xs12 sm6 md6 lg4 xl4style="padding: 5px">
+                      
+                            <v-card-title style="background-color: #a1a1a1;">
+                                <v-layout>
+                                    <v-flex style="text-align: left">
+                                        {{ocorrencia.number}}
+                                    </v-flex>
+                                </v-layout>
+                            </v-card-title>
+                                <v-divider></v-divider>
+                                <v-card-text style="text-align: right; padding-left: 0; padding-top: 0; padding-bottom: 0;  font-style: italic; ">Criado em {{ocorrencia.creation_date}} às {{ocorrencia.creation_time}}</v-card-text>
+                            <v-card-text style="padding-top: 16px;">
+                                <v-layout>
+                                    <v-flex class="fonteCard">
+                                        Local : 
+                                    </v-flex>
+                                    <v-flex class="fonteCard" style="text-align: right">
+                                        {{ocorrencia.logradouro}}
+                                    </v-flex>
+                                </v-layout>
+                            </v-card-text>
+                            <v-card-text style="padding-top: 16px;">
+                                <v-layout>
+                                    <v-flex class="fonteCard">
+                                        Ref: 
+                                    </v-flex>
+                                    <v-flex class="fonteCard" style="text-align: right">
+                                        {{ocorrencia.ponto_referência}}
+                                    </v-flex>
+                                </v-layout>
+                            </v-card-text>
+                            <v-card-text style="padding-top: 16px;" class="fonteCard">
+                                Agentes no local:
+                                <v-chip-group
+                                    multiple
+                                    active-class="primary--text"
+                                >
+                                    <v-chip v-if="ocorrencia.arrived_bptran" color="yellow">
+                                        <span style="color: white">BPTRAN</span>
+                                    </v-chip>
+                                    <v-chip v-if="ocorrencia.arrived_cttu" color="green">
+                                        <span style="color: white">CTTU</span>
+                                    </v-chip>
+                                    <v-chip v-if="ocorrencia.arrived_samu" color="red">
+                                        <span style="color: white">SAMU</span>
+                                    </v-chip>
+                                    <v-chip v-if="ocorrencia.arrived_pm" color="black">
+                                        <span style="color: white">PM</span>
+                                    </v-chip>
+                                    <v-chip v-if="ocorrencia.arrived_prf" color="blue">
+                                        <span style="color: white">PRF</span>
+                                    </v-chip>
+                                    <v-chip v-if="ocorrencia.arrived_bombeiro" color="red">
+                                        <span style="color: white">Bombeiros</span>
+                                    </v-chip>
+                                </v-chip-group>
+                            </v-card-text>
+                            <v-card-actions style="padding-right: 0; padding-top: 3px;"></v-card-actions>
+                            <v-divider></v-divider>
+                            <samu v-if="tipo == 'samu' "> </samu>
+                            <CTTU v-if="tipo == 'cttu' "> </CTTU>
+                  
+                    </v-flex>
+                </v-layout>
+            </v-container>                
+        </v-card-text>
+
     </div>
 
 </template>
@@ -10,6 +78,13 @@
 import Samu from '../components/Samu'
 import CTTU from '../components/CTTU'
 export default {
+    data() {
+        return {
+            ocorrencias: this.$fiery(firebase.firestore().collection('ocorrencias'), {
+                query: q => q.where( this.$store.getters.getUser,'==', true).orderBy('creation_timestamp', 'desc'),
+            })
+        }
+    },
       components: {
         Samu: Samu,
         CTTU: CTTU
@@ -17,8 +92,13 @@ export default {
     computed: {
         ocorrencia () {
             return this.$store.state.selectedOcorrencia
+
         },
         tipo(){
+            return this.$store.getters.getUser
+        },
+         user () {
+            console.log(this.$store.getters.getUser)
             return this.$store.getters.getUser
         }
     },

6931dc96516c189a80ce91b56ef71fae705b66e0
Guilherme Perdigao Lopes <gpl2@WINDOWS.CIN.UFPE.BR>
2019-11-10 12:28:17 -0300

    ocorrencias

--- src/views/Ocorrencias.vue
+++ src/views/Ocorrencias.vue
@@ -1,12 +1,15 @@
 <template>
     <div style="height: 100%; background-color: #FBFBFF;">
-        <v-container style="padding-top: 64px;" >
-            <v-layout mt-4 column >
-                <h3>Eventos abertos</h3>
-                <v-flex xs12 sm6 md6 lg4 xl4 v-for="(ocorrencia, i) in ocorrencias" :key="i" style="padding: 5px; cursor: pointer" @click="goTo(ocorrencia)"  >
-                  
+        <v-container style="padding-bottom: 0; padding-top: 64px; margin-top: 10px;">
+            <v-layout>
+                <v-select hide-details v-model="selectedStatus" :items="status" return-object label="Selecione o status da ocorrência" outlined></v-select>
+            </v-layout>
+        </v-container>
+        <v-container>
+            <v-layout column>
+                <h3>{{events}}</h3>
+                <v-flex xs12 sm6 md6 lg4 xl4 v-for="(ocorrencia, i) in ocorrencias" :key="i" style="padding: 5px; cursor: pointer" @click="goTo(ocorrencia)"  >  
                     <v-card
-                        v-if="ocorrencia.status_bombeiro=='aberto'|| ocorrencia.status_bptran=='aberto'|| ocorrencia.status_cttu=='aberto'|| ocorrencia.status_pm=='aberto'||ocorrencia.status_samu=='aberto'"
                         elevation-5
                         style="border-radius: 10px;"
                         class="mx-auto fonteCard"
@@ -76,171 +79,13 @@
                             </v-chip-group>
                         </v-card-text>
                         <v-card-text style="padding: 10px" v-if="!orgaoPresente(ocorrencia)">
-
+                            <div class="fonteCard">Agentes no local:</div>
+                            Não há agentes presentes no local
                         </v-card-text>
                         <v-card-text style="text-align: right; padding-left: 0; padding-top: 0; padding-bottom: 0; font-weight: 500; font-style: italic; ">Criado em {{ocorrencia.creation_date}} às {{ocorrencia.creation_time}}</v-card-text>
                         <v-card-actions style="padding-right: 0; padding-top: 3px;"></v-card-actions>
                     </v-card>
-                  
                 </v-flex>
-                <h3>Eventos em andamento</h3>
-                    <v-flex xs12 sm6 md6 lg4 xl4 v-for="(ocorrencia, i) in ocorrencias" :key="i" style="padding: 5px; cursor: pointer" @click="goTo(ocorrencia)"  >
-                        <v-card
-                            v-if="ocorrencia.status_bombeiro=='andamento'|| ocorrencia.status_bptran=='andamento'|| ocorrencia.status_cttu=='andamento'|| ocorrencia.status_pm=='andamento'||ocorrencia.status_samu=='andamento'"
-                            elevation-5
-                            style="border-radius: 10px;"
-                            class="mx-auto fonteCard"
-                        >
-                            <v-card-title style="background: #E0E0E0">
-                                <v-layout>
-                                    <v-flex style="text-align: left">
-                                        
-                                        {{ocorrencia.number}}
-                                    </v-flex>
-                                </v-layout>
-                            </v-card-title>
-                            <v-card-text style="padding-top: 16px;">
-                                <v-layout>
-                                    <v-flex class="fonteCard">
-                                        Local : 
-                                    </v-flex>
-                                    <v-flex class="fonteCard" style="text-align: right">
-                                        {{ocorrencia.logradouro}}
-                                    </v-flex>
-                                </v-layout>
-                            </v-card-text>
-                            <v-card-text style="padding-top: 16px;">
-                                <v-layout>
-                                    <v-flex class="fonteCard">
-                                        Ponto de Referência: 
-                                    </v-flex>
-                                    <v-flex class="fonteCard" style="text-align: right">
-                                        {{ocorrencia.ponto_referência}}
-                                    </v-flex>
-                                </v-layout>
-                            </v-card-text>
-                            <v-card-text style="padding-top: 16px;">
-                                <v-layout>
-                                    <v-flex class="fonteCard">
-                                        Quantidade de vítimas: 
-                                    </v-flex>
-                                    <v-flex class="fonteCard" style="text-align: right">
-                                        {{ocorrencia.qtdVitimas}}
-                                    </v-flex>
-                                </v-layout>
-                            </v-card-text>
-                            <v-card-text v-if="orgaoPresente(ocorrencia)" style="padding-top: 16px;" class="fonteCard">
-                                Agentes no local:
-                                <v-chip-group
-                                    multiple
-                                    active-class="primary--text"
-                                >
-                                    <v-chip v-if="ocorrencia.arrived_bptran" color="yellow">
-                                        <span style="color: white">BPTRAN</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_cttu" color="green">
-                                        <span style="color: white">CTTU</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_samu" color="red">
-                                        <span style="color: white">SAMU</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_pm" color="black">
-                                        <span style="color: white">PM</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_prf" color="blue">
-                                        <span style="color: white">PRF</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_bombeiro" color="red">
-                                        <span style="color: white">Bombeiros</span>
-                                    </v-chip>
-                                </v-chip-group>
-                            </v-card-text>
-                            <v-card-text style="padding: 10px" v-if="!orgaoPresente(ocorrencia)">
-
-                            </v-card-text>
-                            <v-card-text style="text-align: right; padding-left: 0; padding-top: 0; padding-bottom: 0; font-weight: 500; font-style: italic; ">Criado em {{ocorrencia.creation_date}} às {{ocorrencia.creation_time}}</v-card-text>
-                            <v-card-actions style="padding-right: 0; padding-top: 3px;"></v-card-actions>
-                        </v-card>
-                    </v-flex>
-                    <h3>Eventos encerrados</h3>
-                    <v-flex xs12 sm6 md6 lg4 xl4 v-for="(ocorrencia, i) in ocorrencias" :key="i" style="padding: 5px; cursor: pointer" @click="goTo(ocorrencia)"  >
-                        <v-card
-                            v-if="ocorrencia.status_bombeiro=='finalizado'|| ocorrencia.status_bptran=='finalizado'|| ocorrencia.status_cttu=='finalizado'||ocorrencia.status_pm=='finalizado'||ocorrencia.status_prf=='finalizado'"
-                            elevation-5
-                            style="border-radius: 10px;"
-                            class="mx-auto fonteCard"
-                        >
-                            <v-card-title style="background: #E0E0E0">
-                                <v-layout>
-                                    <v-flex style="text-align: left">
-                                        
-                                        {{ocorrencia.number}}
-                                    </v-flex>
-                                </v-layout>
-                            </v-card-title>
-                            <v-card-text style="padding-top: 16px;">
-                                <v-layout>
-                                    <v-flex class="fonteCard">
-                                        Local : 
-                                    </v-flex>
-                                    <v-flex class="fonteCard" style="text-align: right">
-                                        {{ocorrencia.logradouro}}
-                                    </v-flex>
-                                </v-layout>
-                            </v-card-text>
-                            <v-card-text style="padding-top: 16px;">
-                                <v-layout>
-                                    <v-flex class="fonteCard">
-                                        Ponto de Referência: 
-                                    </v-flex>
-                                    <v-flex class="fonteCard" style="text-align: right">
-                                        {{ocorrencia.ponto_referência}}
-                                    </v-flex>
-                                </v-layout>
-                            </v-card-text>
-                            <v-card-text style="padding-top: 16px;">
-                                <v-layout>
-                                    <v-flex class="fonteCard">
-                                        Quantidade de vítimas: 
-                                    </v-flex>
-                                    <v-flex class="fonteCard" style="text-align: right">
-                                        {{ocorrencia.qtdVitimas}}
-                                    </v-flex>
-                                </v-layout>
-                            </v-card-text>
-                            <v-card-text v-if="orgaoPresente(ocorrencia)" style="padding-top: 16px;" class="fonteCard">
-                                Agentes no local:
-                                <v-chip-group
-                                    multiple
-                                    active-class="primary--text"
-                                >
-                                    <v-chip v-if="ocorrencia.arrived_bptran" color="yellow">
-                                        <span style="color: white">BPTRAN</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_cttu" color="green">
-                                        <span style="color: white">CTTU</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_samu" color="red">
-                                        <span style="color: white">SAMU</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_pm" color="black">
-                                        <span style="color: white">PM</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_prf" color="blue">
-                                        <span style="color: white">PRF</span>
-                                    </v-chip>
-                                    <v-chip v-if="ocorrencia.arrived_bombeiro" color="red">
-                                        <span style="color: white">Bombeiros</span>
-                                    </v-chip>
-                                </v-chip-group>
-                            </v-card-text>
-                            <v-card-text style="padding: 10px" v-if="!orgaoPresente(ocorrencia)">
-
-                            </v-card-text>
-                            <v-card-text style="text-align: right; padding-left: 0; padding-top: 0; padding-bottom: 0; font-weight: 500; font-style: italic; ">Criado em {{ocorrencia.creation_date}} às {{ocorrencia.creation_time}}</v-card-text>
-                            <v-card-actions style="padding-right: 0; padding-top: 3px;"></v-card-actions>
-                        </v-card>
-                    </v-flex>
             </v-layout>
         </v-container>
     </div>
@@ -251,8 +96,20 @@ export default {
     fiery: true,
     data() {
         return {
-            ocorrencias: this.$fiery(firebase.firestore().collection('ocorrencias'), {
-                query: q => q.where( this.$store.getters.getUser,'==', true).orderBy('creation_timestamp', 'desc'),
+            status: [ 
+                {text: 'Em aberto', value: 'aberto'},
+                {text: 'Em andamento', value: 'andamento'}, 
+                {text: 'Encerrados', value: 'encerrados'}
+            ],
+            selectedStatus: {text: 'Em aberto', value: 'aberto'},
+            aberto: this.$fiery(firebase.firestore().collection('ocorrencias'), {
+                query: q => q.where( this.$store.getters.getUser,'==', true).where('status_' + this.$store.getters.getUser,'==', 'aberto').orderBy('creation_timestamp', 'desc'),
+            }),
+            andamento: this.$fiery(firebase.firestore().collection('ocorrencias'), {
+                query: q => q.where( this.$store.getters.getUser,'==', true).where('status_' + this.$store.getters.getUser,'==', 'andamento').orderBy('creation_timestamp', 'desc'),
+            }),
+            encerrados: this.$fiery(firebase.firestore().collection('ocorrencias'), {
+                query: q => q.where( this.$store.getters.getUser,'==', true).where('status_' + this.$store.getters.getUser,'==', 'encerrados').orderBy('creation_timestamp', 'desc'),
             })
         }
     },
@@ -280,9 +137,21 @@ export default {
     computed: {
         user () {
             console.log(this.$store.getters.getUser)
-            
             return this.$store.getters.getUser
-        }
+        },
+        ocorrencias () {
+            if (this.selectedStatus.value == 'aberto') {
+                return this.aberto
+            }else if (this.selectedStatus.value == 'andamento') {
+                console.log(this.andamento)
+                return this.andamento
+            }else if (this.selectedStatus.value == 'encerrados') {
+                return this.encerrados
+            }
+        },
+        events () {
+           return this.selectedStatus.text 
+        } 
     }
 }
 </script>
